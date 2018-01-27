694a29b8a42f080dff8faa94132330df0b2dd51d
Shaghayegh Taghipour <shaghayegh.taghipour@gmail.com>
2018-01-27 10:55:35 -0800

    add user's controller

--- README.md
+++ README.md
@@ -7,9 +7,11 @@
   - rails g model techStack name  
   - rails g model tagging organization:references tech_stack:references
 * Update Gemfile
+* Update user model
 
   - rails g controller Api::V1::Tokens --no-assets --no-helper --skip-template-engine
-
+  - rails g controller Api::V1::Users --no-assets --no-helper --skip-template-engine
+  - rails g serializer user first_name last_name email id created_at updated_at
 
 This README would normally document whatever steps are necessary to get the
 application up and running.
--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -1,17 +1,17 @@
-class Api::V1::TokensController < ApplicationController
-  def create
+class Api::V1::TokensController < Api::ApplicationController
 
+  def create
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
 
       render json: {
-      jwt: encode_token({
-        id: user.id,
-        first_name: user.first_name,
-        last_name: user.last_name,
-        full_name: user.full_name,
-        email: user.email
-        })
+        jwt: encode_token({
+          id: user.id,
+          first_name: user.first_name,
+          last_name: user.last_name,
+          full_name: user.full_name,
+          email: user.email
+          })
       }
     else
       head :not_found
