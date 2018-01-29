cfa1c326d5c1396525af607f30a6298a9d9561e9
Shaghayegh Taghipour <shaghayegh.taghipour@gmail.com>
2018-01-28 18:06:53 -0800

    seeded the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,120 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+PASSWORD = 'supersecret'
+
+
+TechStack.destroy_all
+Organization.destroy_all
+User.destroy_all
+MeetUp.destroy_all
+Event.destroy_all
+SearchTerm.destroy_all
+Story.destroy_all
+
+#-------------------------------------------------------- TECH-STACK
+['Javascript', 'Ruby', 'Ruby on Rails', 'Node.js', 'React', 'jQuery', 'Bootstrap', 'Postgresql', 'MongoDB'].each do |tech_stack_name|
+  TechStack.create(name: tech_stack_name)
+end
+
+
+tech_stacks = TechStack.all
+puts Cowsay.say("Create #{tech_stacks.count} tech_stacks", :cow)
+
 #
-# Examples:
+# #----------------------------------------------------- Organization
+address = [
+  "155-887 Great Northern Way, Vancouver, BC V5T 1E1",
+  "301-788 Beatty St, Vancouver, BC V6B 2M1",
+  "401-20540 Duncan Way, Langley, BC V3A 7A3",
+  "100-2030 Marine Dr, North Vancouver, BC V7P 1V7",
+  "373-13988 Cambie Rd, Richmond, BC V6V 2K4",
+  "105-19110 24 Ave, Surrey, BC V3Z 3S9",
+  "211 St. Patricks Ave, North Vancouver, BC V7L 3N3",
+  "1687 Nanaimo St, Vancouver, BC V5L 4T9",
+  "6-3490 Kingsway, Vancouver, BC V5R 5L7",
+  "5307 Victoria Dr, Vancouver, BC V5P 3V6"
+]
+
+10.times do
+  Organization.create(
+    name: Faker::Company.name,
+    address: address.sample,
+    overview: Faker::HitchhikersGuideToTheGalaxy.quote,
+    employees: rand(50..1000),
+    team_size: rand(1..10),
+    website: Faker::Internet.url,
+    tech_stacks: [tech_stacks.sample]
+  )
+end
+
+organizations = Organization.all
+puts Cowsay.say("Create #{organizations.count} organizations", :ghostbusters)
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# #-------------------------------------------------------- USER
+super_user = User.create(
+  first_name: 'Jon',
+  last_name: 'Snow',
+  email: 'js@gmail.com',
+  password: PASSWORD,
+  is_admin: true
+)
+
+20.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+
+  User.create(
+    first_name: first_name,
+    last_name: last_name,
+    email: "#{first_name.downcase}.#{last_name.downcase}@example.com",
+    password: PASSWORD,
+    organization: organizations.sample
+  )
+end
+
+users = User.all
+puts Cowsay.say("Create #{users.count} users", :tux)
+#
+# #----------------------------------------------------- Meet-Up
+20.times do
+  MeetUp.create name: Faker::Team.name
+end
+meet_ups = MeetUp.all
+puts Cowsay.say("Create #{meet_ups.count} meet_ups", :cow)
+#
+# #----------------------------------------------------- Event
+e_number = 1
+30.times do
+  Event.create(
+    name: Faker::Dog.name,
+    date: Faker::Date.forward(23),
+    url: Faker::Internet.url,
+    meet_up: MeetUp.all.sample,
+    event_id: e_number += 1)
+end
+events = Event.all
+puts Cowsay.say("Create #{events.count} events", :cow)
+
+
+#
+# #----------------------------------------------------- search_terms
+20.times do
+  SearchTerm.create title: Faker::Coffee.blend_name
+end
+search_terms = SearchTerm.all
+puts Cowsay.say("Create #{search_terms.count} search_terms", :tux)
+#
+# #----------------------------------------------------- stories
+s_number = 1
+50.times do
+  Story.create(
+    title:Faker::Cat.name,
+    date: Faker::Date.forward(60),
+    url: Faker::Internet.url('news.com'),
+    search_term: SearchTerm.all.sample,
+    story_id: s_number += 1)
+end
+
+stories = Story.all
+puts Cowsay.say("Create #{stories.count} stories", :tux)
+
+# #-------------------------------------------------------------------
+puts "Login as admin with #{super_user.email} and password of '#{PASSWORD}'!"
